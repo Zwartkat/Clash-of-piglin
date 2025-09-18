bd380f5baf4e7463ef10a6699947a8b98d92cb67
Zwartkat <zwartkat2@gmail.com>
2025-09-18 18:19:31 +0200

    refactor(entities) : Update entity component class to entity type class

---
+++ src/entities/brute.py
@@ -0,0 +1,6 @@
+from core.entity import Entity
+
+class Brute(Entity):
+    
+    def __init__(self):
+        super().__init__(components=[])
---
+++ src/entities/crossbowman.py
@@ -0,0 +1,6 @@
+from core.entity import Entity
+
+class Crossbowman(Entity):
+    
+    def __init__(self):
+        super().__init__(components=[])
