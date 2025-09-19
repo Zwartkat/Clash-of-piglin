e325df01ffce385d62c898a0271ef5e541c362cc
workai <vaje2005@gmail.com>
2025-09-19 14:42:51 +0200

    fix(economy): Remove SellSystem because it's useless

--- src/events/sell_event.py
+++
@@ -1,4 +0,0 @@
-class SellEvent:
-    def __init__(self, player, entity):
-        self.player = player
-        self.entity = entity
--- src/systems/economy_system.py
+++ src/systems/economy_system.py
@@ -1,13 +1,11 @@
 import esper
 from events.buy_event import BuyEvent
-from events.sell_event import SellEvent
 
 
 class EconomySystem(esper.Processor):
     def __init__(self, event_bus) -> None:
         self.event_bus = event_bus
         event_bus.subscribe(BuyEvent, self.buy)
-        event_bus.subscribe(SellEvent, self.sell)
 
     def buy(self, event):
         player = event.player
@@ -20,12 +18,3 @@ class EconomySystem(esper.Processor):
             )
         else:
             print(f"Il vous manqua {entity.cost - player.money} pour acheter {entity}")
-
-    def sell(self, event):
-        player = event.player
-        entity = event.entity
-        player.money += entity.cost
-        player.entities.remove(entity)
-        print(
-            f"Vous venez de vendre {entity} pour {entity.cost}, vous avez maintenant {player.money} pépites d'or"
-        )
