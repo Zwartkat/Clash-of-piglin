524f374fdbf4fff8931cd8cf2ddaccabef957e86
Scotty Pruvost <scosco09@gmail.com>
2025-09-19 13:54:00 +0200

    feat(player): buy_entity function

--- src/core/player.py
+++ src/core/player.py
@@ -1,5 +1,15 @@
 import esper
+from systems.entity_factory import create
+from event_bus import EventBus
+from events.buy_event import BuyEvent
 
 class Player(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, event_bus) -> None:
+        self.player_entities = []
+        self.money = 600
+        self.event_bus = event_bus
+
+    def buy_entity(self, list_components:list):
+        entity = self.player_entities.append(create(list_components))
+        self.event_bus.emit(BuyEvent(self, entity))
+
