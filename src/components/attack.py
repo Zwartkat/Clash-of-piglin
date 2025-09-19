90c758ebc3ecc6e8668cc8ab33ed9ec3a483365f
MatthieuPinceel <matthieu1.pinceel@gmail.com>
2025-09-19 09:39:00 +0200

    feat(components): add Attack component

--- src/components/attack.py
+++ src/components/attack.py
@@ -8,6 +8,7 @@ class Attack(Component):
     range: int
     attack_speed: float = 1.0
 
-    def __init__(self, damage: int, range: int):
+    def __init__(self, damage: int, range: int, attack_speed: float):
         self.damage = damage
         self.range = range
+        self.attack_speed = attack_speed
