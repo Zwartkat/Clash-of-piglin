1a8e94ff68fc9d650c729a877c89ae0ec8f82982
MatthieuPinceel <matthieu1.pinceel@gmail.com>
2025-09-19 10:32:37 +0200

    feat(components/entities): Update entities and add speed to velocity

--- src/entities/bastion.py
+++ src/entities/bastion.py
@@ -1,3 +1,8 @@
+from components.cost import Cost
+from components.health import Health
+from components.position import Position
+from components.structure import Structure
+from components.attack import Attack
 from core.entity import Entity
 
 
@@ -5,3 +10,8 @@ class Bastion(Entity):
 
     def __init__(self):
         super().__init__(components=[])
+        Structure()
+        Health(health=150)
+        Cost(value=650)
+        Position(x=0, y=0)
+        Attack(damage=0, range=3, attack_speed=0)
--- src/entities/brute.py
+++ src/entities/brute.py
@@ -1,3 +1,8 @@
+from components.attack import Attack
+from components.health import Health
+from components.position import Position
+from components.velocity import Velocity
+from components.cost import Cost
 from core.entity import Entity
 
 
@@ -5,3 +10,8 @@ class Brute(Entity):
 
     def __init__(self):
         super().__init__(components=[])
+        Attack(damage=15, range=1, attack_speed=1.0)
+        Health(health=100)
+        Velocity(x=0, y=0, speed=20)
+        Position(x=0, y=0)
+        Cost(value=350)
