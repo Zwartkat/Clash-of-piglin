2d7bb280734c080a4feb688136812046bcfe08bd
workai <vaje2005@gmail.com>
2025-09-19 10:30:36 +0200

    feat(economy): add EconomySystem and BuyEvent

--- src/systems/economy_system.py
+++ src/systems/economy_system.py
@@ -1,6 +1,20 @@
-import esper    
+import esper
+from events.buy_event import BuyEvent
+
 
 class EconomySystem(esper.Processor):
-    def process(self, dt) -> None:
-        pass
-        # Write gestion of economy
+    def __init__(self, event_bus) -> None:
+        self.event_bus = event_bus
+        event_bus.subscribe(BuyEvent, self.buy)
+
+    def buy(self, event):
+        player = event.player
+        entity = event.entity
+        if player.money >= entity.cost:
+            player.money -= entity.cost
+            player.entities.append(entity)
+            print(
+                f"Player bought {entity} for {entity.cost}. Remaining money: {player.money}"
+            )
+        else:
+            print(f"Il vous manqua {entity.cost - player.money} pour acheter {entity}")
